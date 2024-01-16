8f1bf95598e5da667c61b3a33ef7cd4b95d8d24f
BabyGecko0 <malasialeks1@gmail.com>
2024-01-16 13:09:01 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTraining';
+  buttonClick(){
+    alert("YOU HURT ME!!!");
+  }
+  motivim="we can do it";
+  description="we can do it because we promissed ouselves that we will do it."
+
+  
+
 }
